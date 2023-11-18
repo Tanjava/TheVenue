ed1c66b12015165929a249264be7bb9ecd3aee8f
Tanjava <tanja.pustjens@live.nl>
2023-11-18 19:51:17 +0000

    Setup allauth for authentication

--- thevenue/urls.py
+++ thevenue/urls.py
@@ -19,4 +19,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('restaurant.urls'), name='restaurant_urls'),
+    path('accounts/', include('allauth.urls')),
 ]
