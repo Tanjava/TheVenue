b100b27f2d074bca79822f1369e167148a2e314f
Tanjava <tanja.pustjens@live.nl>
2023-11-19 05:55:11 +0000

    Add reservations page to make a reservation

--- restaurant/views.py
+++ restaurant/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request, 'index.html')
+
+def reservations(request):
+    return render(request, 'reservations.html')
