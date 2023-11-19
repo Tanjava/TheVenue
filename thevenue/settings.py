cf847e9dfda81c4a6c2eb864bd8211005482202e
Tanjava <tanja.pustjens@live.nl>
2023-11-19 11:42:20 +0000

    Add creds and change debug in settings.py

--- thevenue/settings.py
+++ thevenue/settings.py
@@ -28,7 +28,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['the--venue-5dcdc2b376c4.herokuapp.com', '8000-tanjava-thevenue-hjxiidsckof.ws-eu106.gitpod.io']
 
